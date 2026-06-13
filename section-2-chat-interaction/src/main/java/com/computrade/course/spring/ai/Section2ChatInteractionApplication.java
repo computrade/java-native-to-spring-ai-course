caa9f9f1ba7fc98f4d4326c1f1a59b235d8ffb43
yuvalishay <yuvali@computrade.co.il>
2026-06-13 12:37:08 +0300

    First init with Sections

---
+++ section-2-chat-interaction/src/main/java/com/computrade/course/spring/ai/Section2ChatInteractionApplication.java
@@ -0,0 +1,13 @@
+package com.computrade.course.spring.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section2ChatInteractionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section2ChatInteractionApplication.class, args);
+    }
+
+}

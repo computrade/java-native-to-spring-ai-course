78d9ba7c670bd7f626156a5522fd743f3c6ea129
yuvalishay <yuvali@computrade.co.il>
2026-06-15 17:49:06 +0300

    configure `bootRun` task to pass `geminiApiKey` environment variable, add example `gradle.properties` file and update `.gitignore`

--- section-2-chat-gemini/src/main/java/com/computrade/course/spring/ai/controller/GeminiChatController.java
+++ section-2-chat-gemini/src/main/java/com/computrade/course/spring/ai/controller/GeminiChatController.java
@@ -2,12 +2,10 @@ package com.computrade.course.spring.ai.controller;
 
 import com.computrade.course.spring.ai.service.GeminiChatService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/api/gemini")

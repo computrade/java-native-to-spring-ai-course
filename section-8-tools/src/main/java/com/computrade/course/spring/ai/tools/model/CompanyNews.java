8d602777691d08553ecb4749f6aa3e1aff6d86ed
yuvalishay <yuvali@computrade.co.il>
2026-07-29 09:49:54 +0300

    Remove @JsonProperty annotations from CompanyNews

--- section-8-tools/src/main/java/com/computrade/course/spring/ai/tools/model/CompanyNews.java
+++ section-8-tools/src/main/java/com/computrade/course/spring/ai/tools/model/CompanyNews.java
@@ -1,14 +1,13 @@
 package com.computrade.course.spring.ai.tools.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record CompanyNews(
-        @JsonProperty("headline") String headline,
-        @JsonProperty("source") String source,
-        @JsonProperty("url") String url,
-        @JsonProperty("summary") String summary,
-        @JsonProperty("datetime") long datetime
+        String headline, // @JsonProperty("headline") - is not needed as it is same
+        String source,
+        String url,
+        String summary,
+        long datetime
 ) implements MarketTimeAware {
 
     @Override

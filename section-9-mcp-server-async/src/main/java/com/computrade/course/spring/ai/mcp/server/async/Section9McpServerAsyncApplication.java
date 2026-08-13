a2e0233f67926fb51b4534bc09b61b979a1d2421
yuvalishay <yuvali@computrade.co.il>
2026-08-13 10:08:57 +0300

    Rearrange package / modules names

---
+++ section-9-mcp-server-async/src/main/java/com/computrade/course/spring/ai/mcp/server/async/Section9McpServerAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.computrade.course.spring.ai.mcp.server.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section9McpServerAsyncApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section9McpServerAsyncApplication.class, args);
+    }
+
+}
---
+++ section-9-mcp-server-http/src/main/java/com/computrade/course/spring/ai/mcp/server/async/Section9McpServerHttpApplication.java
@@ -0,0 +1,13 @@
+package com.computrade.course.spring.ai.mcp.server.async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section9McpServerHttpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section9McpServerHttpApplication.class, args);
+    }
+
+}

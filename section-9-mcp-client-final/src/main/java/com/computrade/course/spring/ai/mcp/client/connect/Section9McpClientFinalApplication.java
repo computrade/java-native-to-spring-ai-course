c7f2e174fa9d8199c315f45e0d04fe5547d8e84a
yuvalishay <yuvali@computrade.co.il>
2026-08-18 10:55:40 +0300

    Change File name to Final

--- section-9-mcp-client-final/src/main/java/com/computrade/course/spring/ai/mcp/client/connect/Section9McpClientFinalApplication.java
+++ section-9-mcp-client-final/src/main/java/com/computrade/course/spring/ai/mcp/client/connect/Section9McpClientFinalApplication.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class Section9McpClientApplication {
+public class Section9McpClientFinalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Section9McpClientApplication.class, args);
+        SpringApplication.run(Section9McpClientFinalApplication.class, args);
     }
 
 }
